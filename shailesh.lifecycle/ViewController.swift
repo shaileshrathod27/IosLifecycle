fcb872bbaae599e57581220c4337ec68c0f590e7
shaileshrathod27 <saileshrathod27@gmail.com>
2017-06-24 09:41:32 +0530

    Done

--- shailesh.lifecycle/ViewController.swift
+++ shailesh.lifecycle/ViewController.swift
@@ -12,9 +12,32 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        print("viewDidLoad")
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    func viewWillAppear() {
+        
+        print("viewWillAppear")
 
+        
+    }
+    
+    func viewDidAppear() {
+        
+        print("viewDidAppear")
+
+    }
+    
+    func viewWillDisappear() {
+        
+        print("viewWillDisappear")
+
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
